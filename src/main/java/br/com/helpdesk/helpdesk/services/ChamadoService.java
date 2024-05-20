05ab560a1ceb8ee0d7ec0e85128314301915b7ff
JoaoNeto4 <joaoquatlo@hotmail.com>
2024-05-20 19:20:26 -0300

    findAll chamados

--- src/main/java/br/com/helpdesk/helpdesk/services/ChamadoService.java
+++ src/main/java/br/com/helpdesk/helpdesk/services/ChamadoService.java
@@ -1,5 +1,6 @@
 package br.com.helpdesk.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,11 @@ public class ChamadoService {
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+	public List<Chamado> findAll() {
+		return repository.findAll();
 	}
 	
 }
